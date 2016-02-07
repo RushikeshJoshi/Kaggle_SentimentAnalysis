35c026775550ba4167a31548635a6fce15eb7166
Rushikesh Joshi <rsjoshi@caltech.edu>
2016-02-07 00:17:33 -0800

    Added cross-validation

--- analysis.py
+++ analysis.py
@@ -36,7 +36,7 @@ for i in len(data):
 	training_data.append()
 '''
 
-'''
+
 # Create leaf sizes to iterate over
 leaf = []
 for i in range(1, 26):
@@ -47,30 +47,45 @@ depths = []
 for i in range(2, 21):
 	depths.append(i)
 
-# Default is Gini in DTC classifier
-for size in leaf:
-	tree = dtc(min_samples_leaf=size)
-	tree.fit(training[:, :1000], training[:, 1000])
-
-
-# Default is Gini in DTC classifier
-for depth in depths:
-	tree = dtc(max_depth=depth)
-	tree.fit(training[:, :1000], training[:, 1000])
 '''
-
 tree = dtc(min_samples_leaf=10)
 
 tree.fit(X, Y)
 vals = tree.predict(test)
+'''
 
+leaf_errors = []
+depth_errors = []
+
+# Default is Gini in DTC classifier
+for size in leaf:
+	tree = dtc(min_samples_leaf=size)
+	tree.fit(X, Y)
+	scores = cross_val_score(tree, X, Y)
+	leaf_errors.append((scores.mean(), size))
+	print scores.mean()
+
+print "Max lead score: ", max(leaf_errors)
+
+# Default is Gini in DTC classifier
+for depth in depths:
+	tree = dtc(max_depth=depth)
+	tree.fit(X, Y)
+	scores = cross_val_score(tree, X, Y)
+	depth_errors.append((scores.mean(), size))
+
+print "Max depth score: ", max(depth_errors)
+
+
+'''
 output = open("submissions/results.txt", "w")
 output.write("Id,Prediction" + "\n")
 for i in range(1, len(vals) + 1):
 	output.write(str(i) + "," + str(int(vals[i - 1])) + "\n")
+'''
 
 inpFile.close()
-output.close()
+#output.close()
 
 
 
